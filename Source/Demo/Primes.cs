8694c24ad51b297a0125d7221094a9eb449df79a
Ufuk Öz <ufuk.oz@encore.com.tr>
2023-05-19 16:43:02 +0300

    ws3

---
+++ Source/Demo/Primes.cs
@@ -0,0 +1,22 @@
+﻿namespace Prime;
+public static class Primes
+{
+    public static IEnumerable<int> GetPrimes() =>
+        GetPrimeCandidates().Where(IsPrime);
+
+    private static bool IsPrime(int candidate)
+    {
+        return !GetDivisors(candidate).Any();
+    }
+
+    private static IEnumerable<int> GetDivisors(int candidate)
+    {
+        return Enumerable.Range(0, (int)Math.Sqrt(candidate) -1 )
+            .Where(divisor => candidate%divisor == 0);
+    }
+
+    private static IEnumerable<int> GetPrimeCandidates()
+    {
+        return Enumerable.Range(2, int.MaxValue -1);
+    }
+}
